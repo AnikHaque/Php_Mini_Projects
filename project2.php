8ac40b6ac099b3a685c3b04ab4143cc843c30935
AnikHaque <ekramul15-811@diu.edu.bd>
2025-05-07 06:32:24 +0600

    project2 done

---
+++ project2.php
@@ -0,0 +1,9 @@
+<?php
+$celsius = 25;
+$fahrenheit = ($celsius * 9 / 5) + 32;
+echo "$celsius°C is equal to $fahrenheit°F<br>";
+
+$fahrenheitInput = 77;
+$celsiusConverted = ($fahrenheitInput - 32) * 5 / 9;
+echo "$fahrenheitInput°F is equal to $celsiusConverted°C";
+?>
